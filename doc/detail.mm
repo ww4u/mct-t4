0c0fd140603e9607c0626c42c69194bc5212a947
wangzhiyan <wangzhiyan@megarobo.tech>
2018-10-29 14:15:03 +0800

    all

---
+++ doc/detail.mm
@@ -0,0 +1,37 @@
+<map version="1.0.1">
+<!-- To view this file, download free mind mapping software FreeMind from http://freemind.sourceforge.net -->
+<node COLOR="#000000" CREATED="1540793025603" ID="ID_1532717182" MODIFIED="1540793069674" TEXT="detail">
+<font NAME="SansSerif" SIZE="20"/>
+<hook NAME="accessories/plugins/AutomaticLayout.properties"/>
+<node COLOR="#0033ff" CREATED="1540793120125" ID="ID_855536669" MODIFIED="1540793142089" POSITION="right" TEXT="&#x5f00;&#x5173;">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1540793145996" ID="ID_1174198019" MODIFIED="1540793148672" TEXT="&#x63a7;&#x5236;&#x5f00;&#x5173;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1540793154604" ID="ID_1053121863" MODIFIED="1540793172665" TEXT="&#x63a7;&#x5236;&#x6253;&#x5f00;&#x7684;&#x60c5;&#x51b5;&#x4e0b;&#x624d;&#x80fd;&#x8fdb;&#x884c;&#x4e0b;&#x8f7d;&#xff0c;&#x8bfb;&#x53d6;&#x8bbe;&#x7f6e;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+<node COLOR="#00b439" CREATED="1540793131900" ID="ID_1559596972" MODIFIED="1540793143430" TEXT="&#x63a7;&#x5236;&#x5668;&#x5f00;&#x5173;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+<node COLOR="#990000" CREATED="1540793178540" ID="ID_1009759227" MODIFIED="1540793188408" TEXT="&#x5173;&#x95ed;&#x540e;&#x624d;&#x80fd;&#x8fdb;&#x884c;&#x624b;&#x52a8;&#x64cd;&#x4f5c;">
+<font NAME="SansSerif" SIZE="14"/>
+</node>
+</node>
+</node>
+<node COLOR="#0033ff" CREATED="1540793206829" ID="ID_72203820" MODIFIED="1540793210363" POSITION="right" TEXT="home">
+<edge STYLE="sharp_bezier" WIDTH="8"/>
+<font NAME="SansSerif" SIZE="18"/>
+<node COLOR="#00b439" CREATED="1540793210828" ID="ID_476401488" MODIFIED="1540793231321" TEXT="&#x56de;&#x96f6;&#x6709;&#x6548;&#x7684;&#x60c5;&#x51b5;&#x4e0b;&#x624d;&#x80fd;&#x8fdb;&#x884c;&#x540e;&#x7eed;&#x7684;&#x64cd;&#x4f5c;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+<node COLOR="#00b439" CREATED="1540793231806" ID="ID_539182694" MODIFIED="1540793248250" TEXT="&#x56de;&#x96f6;&#x8fc7;&#x7a0b;&#x4e2d;&#xff0c;&#x5207;&#x6362;&#x51fa;&#x56de;&#x96f6;panel,&#x81ea;&#x52a8;&#x505c;&#x6b62;">
+<edge STYLE="bezier" WIDTH="thin"/>
+<font NAME="SansSerif" SIZE="16"/>
+</node>
+</node>
+</node>
+</map>
